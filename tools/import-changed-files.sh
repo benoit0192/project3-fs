259851e7edf1aa92132987c2bb8e0a42d1c98ea8
Benoît Lafon <benoit.lafon92@gmail.com>
2017-04-08 20:01:09 -0500

    update comment for import changed files

--- tools/import-changed-files.sh
+++ tools/import-changed-files.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ "$(uname)" != "Minix" ]; then
-    echo "This script must be executed on your host machine, not in Minix VM"
+    echo "This script must be executed on your Minix VM, not in your host machine"
     echo "Aborting"
     exit 1
 fi
 
 echo "Syncing changed files... "
 
-rsync -c --progress --files-from=/mnt/shared/tools/changed-files.list /mnt/shared /home/repo
+rsync -c --progress --files-from=/mnt/virtual_shared/project3-fs/tools/changed-files.list /mnt/virtual_shared/project3-fs/ /home/repo
 
 echo "[Done]"
